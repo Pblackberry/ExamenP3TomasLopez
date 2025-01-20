bf27c75e4d99bad0c571210bf9f21f186ca62bd8
Pblackberry <tomaslopezandrade2004@gmail.com>
2025-01-20 07:26:08 -0500

    Creacion Modelo

---
+++ ExamenP3TomasLopez/ModelsTLopez/Pais.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace ExamenP3TomasLopez.ModelsTLopez
+{
+    [Table ("Pais")]
+    public class Pais
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [Unique]
+        public string Nombre { get; set; }
+        public string Region { get; set; }
+        public string Link { get; set; }
+        public string TLopez { get; set; }
+    }
+}
